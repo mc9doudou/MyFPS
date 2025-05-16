fc19e2f50a4919089d6974bb96c16f3d23939b4a
DESKTOP-5V3FRMH\M <cof260@gmail.com>
2025-05-16 19:33:04 +0900

    1. 캐릭터 이동 구현

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+namespace MyFPS
+{
+    public class PlayerController : MonoBehaviour
+    {
+        #region Variables
+        //참조 
+        private CharacterController controller;
+
+        //입력 - 이동
+        private Vector2 inputMove;
+        [SerializeField] private float moveSpeed = 10f;
+
+        //중력 - Character Controller필수
+        private float gravity = -9.81f;
+        [SerializeField] private Vector3 velocity;  //중력 계산에 의한 이동 속도
+
+        //그라운드 체크
+        public Transform groundCheck;   //발 바닥 위치
+        [SerializeField] private float checkRange = 0.2f;   //바닥체크 하는 구의 반경 
+        [SerializeField] private LayerMask groundMask;      //그라운드 레이어 판별
+
+        //점프 높이
+        [SerializeField] private float jumpHeight = 1f;
+        #endregion
+
+        #region Unity Event Mathod
+        private void Start()
+        {
+            //참조 
+            controller = this.GetComponent<CharacterController>();
+        }
+        private void Update()
+        {
+            //땅에 있으면 
+            bool isGrounded = GroundCheck();
+            if (isGrounded && velocity.y < 0f)
+            {
+                velocity.y = -10f;
+            }
+
+            //방향 (Global축) 이동
+            //Vector3 moveDir = Vector3.right * inputMove.x + Vector3.forward * inputMove.y;
+            //방향 (Local 축) 이동
+            Vector3 moveDir = transform.right * inputMove.x + transform.forward * inputMove.y;
+
+            //이동
+            controller.Move(moveDir * Time.deltaTime * moveSpeed);
+
+            //중력으로 인한 하강이동
+            velocity.y += gravity * Time.deltaTime;
+            controller.Move(velocity * Time.deltaTime);
+        }
+        #endregion
+
+        #region Custom Mathod
+        //Input System에 등록할 함수
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            inputMove = context.ReadValue<Vector2>();
+        }
+        public void OnJump(InputAction.CallbackContext context)
+        {
+            if (context.started && GroundCheck())
+            {
+                velocity.y = Mathf.Sqrt(-2f * gravity * jumpHeight);
+            }
+        }
+
+        //그라운드 체크
+        bool GroundCheck()
+        {
+            return Physics.CheckSphere(groundCheck.position, checkRange, groundMask);
+        }
+        #endregion
+
+
+    }
+}
---
+++ Assets/_Sample/02AnimationTest/PlayerAniTest.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace MySample
+{
+    public class PlayerAniTest : MonoBehaviour
+    {
+        
+
+    }
+}
+/*
+1. WASD 입력 받아서 플레이어 앞뒤좌우로 이동
+2. 앞뒤죄우 이동시 앞뒤좌우 애니메이션 플레이 시켜준다
+3. 이동이 없을 때에는 대기 애니메이션 플레이 한다
+4. 이동속도 5
+
+
+*/
